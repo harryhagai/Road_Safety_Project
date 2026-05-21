db536e05762592ca03ee8b41225f357d36d4c6f1
harryhagai <hngobey@gmail.com>
2026-05-21 20:21:07 +0300

    fix gps issue

--- app/Http/Controllers/AutoSpeedReportController.php
+++ app/Http/Controllers/AutoSpeedReportController.php
@@ -19,7 +19,7 @@ class AutoSpeedReportController extends Controller
     private const REQUIRED_EXCEEDED_SECONDS = 30;
     private const DUPLICATE_WINDOW_SECONDS = 600;
     private const BASE_SEGMENT_TOLERANCE_METERS = 30;
-    private const MAX_SEGMENT_TOLERANCE_METERS = 1;
+    private const MAX_SEGMENT_TOLERANCE_METERS = 6;
 
     /**
      * Handle the evaluate workflow for this class.
